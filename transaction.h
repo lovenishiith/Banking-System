d5216c8013aae7bb6daf1b42ccd59ea8931c142a
lovenishiith <lovenishiith03@gmail.com>
2026-04-23 15:16:02 +0530

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -1,9 +1,13 @@
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
+
+#include <string>
+using namespace std;
+
 // Transaction operations
-void deposit(int user_id);
-void withdraw(int user_id);
-void transfer(int user_id);
+void deposit(int user_id, double amount, string note);
+void withdraw(int user_id, double amount, string note);
+void transfer(int sender_id);
 
 // Viewing data
 void viewTransactions(int user_id);
@@ -12,4 +16,5 @@ void CheckBalance(int user_id);
 // Analytics
 void showAccountSummary(int user_id);
 void showMonthlySpending(int user_id);
+
 #endif
